cb86b1212084a63825880ba19beb8f6293c430cb
Davide Brescia <61413670+isBre@users.noreply.github.com>
2024-10-16 10:47:35 +0200

    removed unused newlines

--- src/Utils.py
+++ src/Utils.py
@@ -13,7 +13,6 @@ def fix_random(seed: int) -> None:
     random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
