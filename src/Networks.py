987062d258a26bc9d03ac52c3004631fb28bdfc4
Davide Brescia <61413670+isBre@users.noreply.github.com>
2024-10-16 10:36:00 +0200

    remove unused newlines

--- src/Networks.py
+++ src/Networks.py
@@ -11,9 +11,7 @@ class SiameseNetwork(Module):
 
     def __init__(self, output : int, backbone: models):
         super(SiameseNetwork, self).__init__()
-
         self.backbone = backbone
-        
         self.backbone.fc = Sequential (
             Linear(self.backbone.fc.in_features, 256),
             ReLU(inplace = True),
@@ -22,20 +20,15 @@ class SiameseNetwork(Module):
             Linear(128, output)
         )
 
-
     def forward_once(self, x : torch.Tensor):
         return self.backbone(x)
 
-
     def forward(self, inputs : torch.Tensor):
-
         if len(inputs) == 2:
             return torch.stack((self.forward_once(inputs[0]),
                                 self.forward_once(inputs[1])))
-        
         if len(inputs) == 3:
             return torch.stack((self.forward_once(inputs[0]),
                                 self.forward_once(inputs[1]),
                                 self.forward_once(inputs[2]),))
-
         raise ValueError(f'This is the SiameseBaseline, the number of input tensor is not 2 or 3 {inputs.size()}')
