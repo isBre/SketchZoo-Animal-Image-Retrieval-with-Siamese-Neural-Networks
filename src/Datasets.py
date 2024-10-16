4ec6537e758f614a69fd7893a240397c90f8d930
Davide Brescia <61413670+isBre@users.noreply.github.com>
2024-10-16 10:29:50 +0200

    improved readabilty + code style

--- src/Datasets.py
+++ src/Datasets.py
@@ -6,12 +6,16 @@ import random
 
 class ContrastiveDataset(Dataset):
 
-    def __init__(self, images_ds : Dataset, sketches_ds : Subset, transform : transforms = None):
+    def __init__(
+        self, 
+        images_ds: Dataset, 
+        sketches_ds: Subset, 
+        transform: transforms=None,
+    ) -> None:
         self.images_ds = images_ds
         self.sketches_ds = sketches_ds
         self.transform = transform
         self.sketches_dict = self.create_sketches_dict()
-
 
     def create_sketches_dict(self):
         class_indices = {}
@@ -21,53 +25,62 @@ class ContrastiveDataset(Dataset):
             class_indices[label].append(idx)
         return class_indices
         
-
-    def __getitem__(self, index):
+    def __getitem__(self, index: int):
         image, image_class = self.images_ds[index]
-
         #We need to approximately 50% of images to be in the same class
         should_get_same_class = random.randint(0, 1)
-
         if should_get_same_class:
             idx = random.choice(self.sketches_dict[image_class])
-        
         else:
-            random_class = random.choice([idx for idx in list(self.sketches_dict.keys()) if idx != image_class])
+            random_class = random.choice(
+                [idx for idx in list(self.sketches_dict.keys()) if idx != image_class]
+            )
             idx = random.choice(self.sketches_dict[random_class])
-        
         if self.transform:
             image = self.transform(image)
         sketch, sketch_class = self.sketches_ds[idx]
         inputs = torch.stack((image, sketch))
-
         # Indicate whether a pair of samples (input1 and input2) are similar or dissimilar
         # 0: are the same class, 1: different class
         target = torch.tensor([(image_class != sketch_class)]).long()
-        
         return inputs, target
-    
 
     def __len__(self):
         return len(self.images_ds)
 
 
 class AugmentedContrastiveDataset(ContrastiveDataset):
-  def __init__(self, images_ds : Dataset, sketches_ds : Subset, transform : transforms = None):
-    super().__init__(images_ds, sketches_ds, transform)
+    
+    def __init__(
+        self, 
+        images_ds: Dataset, 
+        sketches_ds: Subset, 
+        transform: transforms=None,
+    ) -> None:
+        super().__init__(images_ds, sketches_ds, transform)
 
-  def __getitem__(self, idx):
-    input, target = super().__getitem__(idx)
-    return torch.stack((self.transform(input[0]), self.transform(input[1]))), target
+    def __getitem__(self, index: int):
+        input, target = super().__getitem__(index)
+        return (torch.stack((
+            self.transform(input[0]), 
+            self.transform(input[1]))
+            ), 
+            target
+        )
 
 
 class TripletDataset(Dataset):
 
-    def __init__(self, images_ds : Dataset, sketches_ds : Subset, transform : transforms = None):
+    def __init__(
+        self,
+        images_ds: Dataset,
+        sketches_ds: Subset,
+        transform: transforms=None,
+    ) -> None:
         self.images_ds = images_ds
         self.sketches_ds = sketches_ds
         self.transform = transform
         self.sketches_dict = self.create_sketches_dict()
-    
 
     def create_sketches_dict(self):
         class_indices = {}
@@ -77,33 +90,42 @@ class TripletDataset(Dataset):
             class_indices[label].append(idx)
         return class_indices
 
-
-    def __getitem__(self, index):
+    def __getitem__(self, index: int):
         image, image_class = self.images_ds[index]
-
         positive_idx = random.choice(self.sketches_dict[image_class])
-        random_negative_class = random.choice([idx for idx in list(self.sketches_dict.keys()) if idx != image_class])
+        random_negative_class = random.choice(
+            [idx for idx in list(self.sketches_dict.keys()) if idx != image_class]
+        )
         negative_idx = random.choice(self.sketches_dict[random_negative_class])
-          
         positive_sketch, _, = self.sketches_ds[positive_idx]
         negative_sketch, _ = self.sketches_ds[negative_idx]
-
         inputs = torch.stack((image, positive_sketch, negative_sketch))
         # Indicate whether a pair of samples (input1 and input2) are similar or dissimilar
         # 0: are the same class, 1: different class
         target = torch.tensor([0, 1])
-        
         return inputs, target
-    
 
     def __len__(self):
         return len(self.images_ds)
     
 
 class AugmentedTripletDataset(TripletDataset):
-  def __init__(self, images_ds : Dataset, sketches_ds : Subset, transform : transforms = None):
-    super().__init__(images_ds, sketches_ds, transform)
-
-  def __getitem__(self, idx):
-    input, target = super().__getitem__(idx)
-    return torch.stack((self.transform(input[0]), self.transform(input[1]), self.transform(input[2]))), target
+    
+    def __init__(
+        self, 
+        images_ds: Dataset, 
+        sketches_ds: Subset, 
+        transform: transforms=None,
+    ) -> None:
+        super().__init__(images_ds, sketches_ds, transform)
+    
+    def __getitem__(self, index: int):
+        input, target = super().__getitem__(index)
+        return (
+            torch.stack((
+                self.transform(input[0]), 
+                self.transform(input[1]), 
+                self.transform(input[2]))
+            ), 
+            target
+        )
